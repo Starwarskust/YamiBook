6a591c4adb0ffb392235f90775f6888ff8aad84f
rustam <starwarskust@gmail.com>
2025-11-02 23:27:51 +0300

    move main screen to compose class

---
+++ app/src/main/java/ru/yamibook/YamiBookApp.kt
@@ -0,0 +1,29 @@
+package ru.yamibook
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import ru.yamibook.ui.theme.YamiBookTheme
+
+@Composable
+fun YamiBookApp() {
+    YamiBookTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}

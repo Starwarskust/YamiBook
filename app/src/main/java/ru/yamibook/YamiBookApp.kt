0778e9dc821d05c41009e658cba6c8465dead3fd
rustam <starwarskust@gmail.com>
2025-11-03 16:01:19 +0300

    add top bar

--- app/src/main/java/ru/yamibook/YamiBookApp.kt
+++ app/src/main/java/ru/yamibook/YamiBookApp.kt
@@ -2,16 +2,51 @@ package ru.yamibook
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AddCircle
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ru.yamibook.ui.theme.Pink
 import ru.yamibook.ui.theme.YamiBookTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun YamiBookApp() {
     YamiBookTheme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text(stringResource(R.string.recipes)) },
+                    actions = {
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Search,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        IconButton(onClick = { /* do something */ }) {
+                            Icon(
+                                imageVector = Icons.Outlined.AddCircle,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                    },
+                    colors = topAppBarColors(
+                        containerColor = Pink
+                    ),
+                )
+            },
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
             Greeting(
                 name = "Android",
                 modifier = Modifier.padding(innerPadding)
